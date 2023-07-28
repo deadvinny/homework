537d216943c4efc25e3629c303de0b6e8a847864
Polina <deadvinny1@gmail.com>
2023-07-28 16:58:54 +0300

    Добавлена задача 47

---
+++ task47/Program.cs
@@ -0,0 +1,43 @@
+﻿// Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+
+// m = 3, n = 4.
+// 0,5 7 -2 -0,2
+// 1 -3,3 8 -9,9
+// 8 7,8 -7,1 9
+
+void PrintDoubleMassive(double[,] massive)
+{
+    for (int i = 0; i < massive.GetLength(0); i++) // (0) - строки (длинна строк массива)
+    {
+        for (int j = 0; j < massive.GetLength(1); j++) // (1) - столцы (длинна столбцов массива)
+        {
+            Console.Write($"{massive[i,j]} ");
+        }
+    Console.WriteLine();
+    }
+}
+
+double[,] DoDoubleMassive(int rows, int columns, double minValue, double maxValue)
+{
+    double[,] result = new double [rows, columns];
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            result[i, j] = Math.Round(new Random().NextDouble()*9, 1);
+        }
+    }    
+    return result;
+}
+
+int GetInfo(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int rows = GetInfo("Введите количество строк массива: ");
+int columns = GetInfo("Введите количество столбцов массива: ");
+
+double[,] massive = DoDoubleMassive(rows, columns, 0, 9);
+PrintDoubleMassive(massive);
